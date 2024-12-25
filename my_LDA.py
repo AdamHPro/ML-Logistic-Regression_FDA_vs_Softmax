6acec8971ec833d9cf79c8a2bd04c1d517063413
AdamHPro <adam.hachicha@student-cs.fr>
2024-12-25 16:45:38 +0100

    Add files via upload

---
+++ my_LDA.py
@@ -0,0 +1,72 @@
+import numpy as np
+import scipy.linalg as linalg
+
+def my_LDA(X, Y):
+    """
+    Train a LDA classifier from the training set
+    X: training data
+    Y: class labels of training data
+
+    """    
+    classLabels = np.unique(Y)  # different class labels on the dataset
+    classNum = len(classLabels)
+    datanum, dim = X.shape  # dimensions of the dataset
+    totalMean = np.mean(X, 0)  # total mean of the data
+    totalMean = totalMean.T
+
+    #Moyenne au sein de chaque classe :
+    
+    means = {classe : np.zeros(dim,) for classe in classLabels}
+    counting = {classe : 0 for classe in classLabels}
+    
+    for k in range(datanum) :
+        x = X[k,:]
+        classe = Y[k]
+        means[classe] += x.T
+        counting[classe] += 1
+        
+    for classe in means :
+        means[classe] = means[classe]/counting[classe]
+        
+    #Calcul de la matrice représentant la variance au sein de chaque classe.
+    Sw = np.zeros((dim, dim))
+    for k in range(datanum) :
+        x = X[k,:]
+        classe = Y[k]
+        #Sw += np.matmul(x.T - means[classe], (x.T- means[classe]).T)
+        diff = x.T - means[classe]
+        Sw += np.outer(diff, diff)
+
+        
+    #Calcul de la matrice représentant la différence des moyennes.
+    
+    Sb = np.zeros((dim, dim))
+    for classe in means :
+        Sb += counting[classe]*np.matmul(means[classe]-totalMean, (means[classe]-totalMean).T)
+        
+    #Calcul de W
+    
+    A = np.matmul(linalg.inv(Sw), Sb)
+    values, vectors = linalg.eig(A)
+
+    # On trie les valeurs propres en ordre décroissant
+    sorted_indices = np.argsort(-values.real)  
+    values = values[sorted_indices]
+    vectors = vectors[:, sorted_indices]
+    W = vectors[:, :len(classLabels)-1]
+    
+    X_lda = np.matmul(X, W)
+    projected_centroid = np.array([np.matmul(W.T, means[classe]) for classe in means])
+        
+        
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Implement the LDA technique, following the
+    # steps given in the pseudocode on the assignment.
+    # The function should return the projection matrix W,
+    # the centroid vector for each class projected to the new
+    # space defined by W and the projected data X_lda.
+
+
+    # =============================================================
+
+    return W, projected_centroid, X_lda
---
+++ predict.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def predict(X, projected_centroid, W):
+    """Apply the trained LDA classifier on the test data
+    X: test data
+    projected_centroid: centroid vectors of each class projected to the new space
+    W: projection matrix computed by LDA
+    """
+
+    # Project test data onto the LDA space defined by W
+    projected_data = np.dot(X, W)
+    label = []
+    for x in projected_data:
+        classe = 0
+        distance = np.inner(x - projected_centroid[0], x - projected_centroid[0])
+        for i in range(1, len(projected_centroid)) :
+            new_dist = np.inner(x - projected_centroid[1], x - projected_centroid[1])
+            if distance > new_dist :
+                classe = i
+                distance = new_dist
+        label.append(classe)
+    
+            
+        
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Fill in the code to implement the classification
+    # part of LDA. Follow the steps given in the assigment.
+
+    # =============================================================
+
+    # Return the predicted labels of the test data X
+    return np.array(label)
